f0a3863c499394aa397f0eb57a1fc0899f272d3a
Jeff Seymour and Kyle Langley <seym1585@gmail.com>
2016-01-11 16:16:01 -0800

    add test for equal hash

--- lib/EHash.rb
+++ lib/EHash.rb
@@ -5,6 +5,7 @@ class EHash
   end
 
   define_method(:store) do |key, value|
+    return if key.class == NilClass || value.class == NilClass 
     @Keys.push(key)
     @Values.push(value)
   end
@@ -17,6 +18,27 @@ class EHash
     end
   end
 
+  define_method(:is_equal?) do |hash|
+    for i in 0..@Keys.length
+      key_value = hash.get_key_value_by_index( i )
+      if @Keys[ i ] != key_value[ 0 ] || @Values[ i ] != key_value[ 1 ]
+        return false
+      end
+    end
+    return true
+  end
+
+  define_method(:get_key_value_by_index) do |index|
+    key_value = Array.new( )
+    for i in 0..@Keys.length
+      if i == index
+        key_value.push( @Keys[ i ] )
+        key_value.push( @Values[ i ] )
+        return key_value
+      end
+    end
+  end
+
   define_method(:has_key?) do |key|
     return @Keys.include?(key)
   end
@@ -25,4 +47,23 @@ class EHash
     return @Values.include?(value)
   end
 
+  define_method(:length) do
+    return @Keys.length
+  end
+
+  define_method(:merge) do |hash|
+    merged_hash = EHash.new()
+
+    for j in 0..hash.length
+      first_key_value = hash.get_key_value_by_index( j )
+      merged_hash.store( first_key_value[ 0 ], first_key_value[ 1 ] )
+    end
+
+    for i in 0..@Keys.length
+      second_key_value = get_key_value_by_index( i )
+      merged_hash.store( second_key_value[ 0 ], second_key_value[ 1 ] )
+    end
+
+    return merged_hash
+  end
 end
--- spec/EHash_spec.rb
+++ spec/EHash_spec.rb
@@ -25,4 +25,23 @@ describe(EHash) do
       expect(test_hash.has_value?("cute")).to(eq(true))
     end
   end
+
+  describe("#length") do
+    it("checks hash for it's length") do
+      test_hash = EHash.new()
+      test_hash.store("kitten", "cute")
+      expect(test_hash.length()).to(eq(1))
+    end
+  end
+
+  describe("#merge") do
+    it("merge two EHash groups and return result") do
+      test_hash = EHash.new()
+      test_hash.store("kitten", "cute")
+      second_test_hash = EHash.new( )
+      second_test_hash.store("dog", "fat")
+      merged = test_hash.merge(second_test_hash)
+      expect(merged.length).to(eq(2))
+    end
+  end
 end
